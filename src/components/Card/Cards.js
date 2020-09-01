61b847b59b9c15980d4fbd45f3ee3f6549fe90a8
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-01 14:43:18 +0400

    feat: add id and selectedId

--- src/components/Card/Cards.js
+++ src/components/Card/Cards.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import TheCard from "./TheCard";
 
@@ -7,6 +7,7 @@ import "../../assets/stylesheets/TheCard.css";
 const Cards = () => {
   const products = [
     {
+      id: 1,
       taste: "фуа-гра",
       weight: 0.5,
       count: 10,
@@ -14,6 +15,7 @@ const Cards = () => {
       isHappy: false,
     },
     {
+      id: 2,
       taste: "рыбой",
       weight: 2,
       count: 40,
@@ -21,6 +23,7 @@ const Cards = () => {
       isHappy: false,
     },
     {
+      id: 3,
       taste: "курой",
       weight: 5,
       count: 100,
@@ -29,15 +32,20 @@ const Cards = () => {
     },
   ];
 
+  const [selectedId, setSelectedId] = useState(null);
+
   const renderCardList = products.map((product) => {
     return (
       <TheCard
-        key={product.taste}
+        key={product.id}
+        id={product.id}
+        selectedId={selectedId}
         taste={product.taste}
         weight={product.weight}
         count={product.count}
         giftCount={product.giftCount}
         isHappy={product.isHappy}
+        setSelectedId={setSelectedId}
       />
     );
   });
