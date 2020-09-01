37286391f7c5e4e09fe9fdeac63cedccea01b8d8
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-01 11:39:25 +0400

    feat: import style and cards component

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,15 @@
 import React from 'react';
 
+import "../assets/stylesheets/App.css";
+import Cards from './Card/Cards';
+
 const App = () => {
-  return <h1>App</h1>
+  return (
+    <div>
+      <h1 className="page__header">Ты сегодня покормил кота?</h1>
+      <Cards />
+    </div>
+  )
 }
 
 export default App;
