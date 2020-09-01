2a9854332d8839ed3f7f316af10f8187b49b0762
dvnikolaev <dvnikolaeev@gmail.com>
2020-09-01 23:09:11 +0400

    Updates

---
+++ precache-manifest.a19bedfaea8f431dd1f5a86d034e6ebc.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3af90ee9a75417e05f9bb1f0dcd75dce",
+    "url": "/react-cat-food/index.html"
+  },
+  {
+    "revision": "26be48389b29dda7cd46",
+    "url": "/react-cat-food/static/css/main.24535aa3.chunk.css"
+  },
+  {
+    "revision": "7c52ab0c2cab015ad72a",
+    "url": "/react-cat-food/static/js/2.bdda9f34.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-cat-food/static/js/2.bdda9f34.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "26be48389b29dda7cd46",
+    "url": "/react-cat-food/static/js/main.f6a023aa.chunk.js"
+  },
+  {
+    "revision": "789786de9fb31fca7161",
+    "url": "/react-cat-food/static/js/runtime-main.6b6343ad.js"
+  },
+  {
+    "revision": "44bde8744ccc0de7995a23b0e82dd281",
+    "url": "/react-cat-food/static/media/Exo2.0-Thin.44bde874.otf"
+  },
+  {
+    "revision": "3d7e1ad70582e5c2e1e79c70f54739ad",
+    "url": "/react-cat-food/static/media/bg.3d7e1ad7.png"
+  },
+  {
+    "revision": "b63f45d400b8eaf15a696275935dc7f1",
+    "url": "/react-cat-food/static/media/cat.b63f45d4.png"
+  }
+]);
